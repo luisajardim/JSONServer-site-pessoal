9898968362030be29c22a76686389354c858f324
Luísa Jardim <luisaojardim@gmail.com>
2024-06-26 17:58:25 -0300

    dando errado

--- index.js
+++ index.js
@@ -1,28 +1,110 @@
-// Trabalho Interdisciplinar 1 - Aplicações Web
-//
-// Esse módulo implementa uma API RESTful baseada no JSONServer
-// O servidor JSONServer fica hospedado na seguinte URL
-// https://jsonserver.rommelpuc.repl.co/contatos
-//
-// Para montar um servidor para o seu projeto, acesse o projeto 
-// do JSONServer no Replit, faça o FORK do projeto e altere o 
-// arquivo db.json para incluir os dados do seu projeto.
-//
-// URL Projeto JSONServer: https://replit.com/@rommelpuc/JSONServer
-//
-// Autor: Rommel Vieira Carneiro
-// Data: 03/10/2023
+const axios = require('axios');
+const fs = require('fs');
 
-const jsonServer = require('json-server')
-const server = jsonServer.create()
-const router = jsonServer.router('./db/db.json')
+const userProfileURL = 'https://api.github.com/users/luisajardim';
+const userReposURL = 'https://api.github.com/users/luisajardim/repos';
+const teamMemberURLs = [
+  'https://api.github.com/users/CarlosJFigueiredo',
+  'https://api.github.com/users/joaogscc',
+  'https://api.github.com/users/gabialvarenga',
+  'https://api.github.com/users/marcosffp'
+];
 
-// Para permitir que os dados sejam alterados, altere a linha abaixo
-// colocando o atributo readOnly como false.
-const middlewares = jsonServer.defaults()
+const carouselImages = [
+  { src: '/public/assets/img/img/banner__1.png', alt: 'mulheres' },
+  { src: '/public/assets/img/img/banner__2.png', alt: 'demencia digital' },
+  { src: '/public/assets/img/img/banner__3.jpeg', alt: 'ada lovelace' },
+  { src: '/public/assets/img/img/banner__4.webp', alt: 'allura' },
+  
+];
 
-server.use(middlewares)
-server.use(router)
-server.listen(3000, () => {
-  console.log('JSON Server está em execução!')
-})
+async function getProfileData(url) {
+  try {
+    const response = await axios.get(url);
+    return response.data;
+  } catch (error) {
+    console.error(`Erro ao obter dados do perfil de ${url}:`, error);
+    return null;
+  }
+}
+
+async function getRepoData(url) {
+  try {
+    const response = await axios.get(url);
+    return response.data;
+  } catch (error) {
+    console.error('Erro ao obter dados dos repositórios:', error);
+    return null;
+  }
+}
+
+async function getTeamData(urls) {
+  const teamDataPromises = urls.map(url => axios.get(url));
+  try {
+    const teamDataResponses = await Promise.all(teamDataPromises);
+    return teamDataResponses.map(response => response.data);
+  } catch (error) {
+    console.error('Erro ao obter dados da equipe:', error);
+    return null;
+  }
+}
+
+async function salvarDados() {
+  const dadosusuario = await getProfileData(userProfileURL);
+  const dadosRepositorios = await getRepoData(userReposURL);
+  const dadosEquipe = await getTeamData(teamMemberURLs);
+
+  if (dadosusuario && dadosRepositorios && dadosEquipe) {
+    const githubUser = {
+      id: dadosusuario.id,
+      login: dadosusuario.login,
+      name: dadosusuario.name,
+      bio: dadosusuario.bio,
+      location: dadosusuario.location,
+      followers: dadosusuario.followers,
+      following: dadosusuario.following,
+      public_repos: dadosusuario.public_repos,
+      public_gists: dadosusuario.public_gists,
+      avatar_url: dadosusuario.avatar_url,
+      html_url: dadosusuario.html_url,
+      blog: dadosusuario.blog
+    };
+
+    const repositoriosArray = dadosRepositorios.map(repo => ({
+      id: repo.id,
+      name: repo.name,
+      full_name: repo.full_name,
+      description: repo.description,
+      html_url: repo.html_url,
+      stargazers_count: repo.stargazers_count,
+      watchers_count: repo.watchers_count,
+      language: repo.language,
+      forks_count: repo.forks_count,
+      open_issues_count: repo.open_issues_count,
+      created_at: repo.created_at,
+      topics: repo.topics
+    }));
+
+    const equipeArray = dadosEquipe.map(member => ({
+      id: member.id,
+      login: member.login,
+      name: member.name,
+      avatar_url: member.avatar_url,
+      html_url: member.html_url
+    }));
+
+    const dadosFinais = {
+      usuario: githubUser,
+      repositorios: repositoriosArray,
+      equipe: equipeArray,
+      carrossel: carouselImages
+    };
+
+    fs.writeFileSync('./db/db.json', JSON.stringify(dadosFinais, null, 2));
+    console.log('Dados do GitHub adicionados ao arquivo db.json com sucesso!');
+  } else {
+    console.log('Erro ao obter dados do usuário, dos repositórios ou da equipe no GitHub');
+  }
+}
+
+salvarDados();
--- public/assets/js/app.js
+++ public/assets/js/app.js
@@ -1,86 +1,79 @@
 document.addEventListener('DOMContentLoaded', () => {
-    const userProfileURL = 'https://api.github.com/users/gabialvarenga';
-    const userReposURL = 'https://api.github.com/users/gabialvarenga/repos';
-    const teamMemberURLs = [
-      'https://api.github.com/users/CarlosJFigueiredo',
-      'https://api.github.com/users/joaogscc',
-      'https://api.github.com/users/luisajardim'
-    ];
-  
-    const carouselImages = [
-      { src: '/public/assets/img/imagem-chatbot.png', alt: 'tecnologia' },
-      { src: '/public/assets/img/imagem-iot.png', alt: 'inovação' },
-      { src: '/public/assets/img/imagem-realidade-virtual.jpg', alt: 'inteligência artificial' },
-      { src: '/public/assets/img/imagem_ia.jpeg', alt: 'desenvolvimento' },
-      { src: '/public/assets/img/imagem-blockchain.png', alt: 'tecnologia emergente' }
-    ];
-  
-    // Função para obter dados dos repositórios do usuário
-  async function getRepoData() {
+  const userProfileURL = 'https://api.github.com/users/luisajardim';
+  const userReposURL = 'https://api.github.com/users/luisajardim/repos';
+  const teamMemberURLs = [
+    'https://api.github.com/users/CarlosJFigueiredo',
+    'https://api.github.com/users/gabialvarenga',
+    'https://api.github.com/users/marcosffp'
+  ];
+
+  const carouselImages = [
+    { src: '/public/assets/img/img/banner__1.png', alt: 'mulheres' },
+    { src: '/public/assets/img/img/banner__2.png', alt: 'demencia digital' },
+    { src: '/public/assets/img/img/banner__3.jpeg', alt: 'ada lovelace' },
+    { src: '/public/assets/img/img/banner__4.webp', alt: 'allura' },
+  ];
+
+  // Função para obter dados do perfil do usuário
+  async function getProfileData(url, elementId) {
     try {
+      const response = await fetch(url);
+      const profileData = await response.json();
+
+      const profileHTML = `
+        <img src="${profileData.avatar_url}" alt="Avatar de ${profileData.login}" class="img-fluid rounded-circle mb-3">
+        <h1>${profileData.name}</h1>
+        <p>${profileData.bio}</p>
+        <a href="${profileData.html_url}" class="btn btn-primary" target="_blank">Ver perfil no GitHub</a>
+      `;
+
+      document.getElementById(elementId).innerHTML = profileHTML;
+    } catch (error) {
+      console.error('Erro ao obter dados do perfil:', error);
+    }
+  }
+
+ // Função para obter dados dos repositórios do usuário
+async function getRepoData() {
+  try {
       const response = await fetch(userReposURL);
       const reposData = await response.json();
-  
+
       const reposHTML = reposData.map(repo => `
-        <div class="col-md-4 col-sm-6">
-          <div class="card">
-            <div class="card-body">
-              <div class="d-flex justify-content-between">
-                <h5 class="card-title">${repo.name}</h5>
-                <div class="text-end">
-                  <ul class="list-inline">
-                    <li class="list-inline-item">Estrelas: ${repo.stargazers_count}</li>
-                    <li class="list-inline-item">Forks: ${repo.forks_count}</li>
-                  </ul>
-                </div>
+          <div class="col-md-4 col-sm-6 repo-card">
+              <div class="card">
+                  <div class="card-body">
+                      <div class="d-flex justify-content-between">
+                          <h5 class="card-title">${repo.name}</h5>
+                          <div class="text-end">
+                              <ul class="list-inline">
+                                  <li class="list-inline-item">Estrelas: ${repo.stargazers_count}</li>
+                                  <li class="list-inline-item">Forks: ${repo.forks_count}</li>
+                              </ul>
+                          </div>
+                      </div>
+                      <p class="card-text">${repo.description || 'Sem descrição disponível.'}</p>
+                      <a href="repo.html?repo=${repo.name}" class="btn btn-primary" target="_blank">Ver repositório</a>
+                  </div>
               </div>
-              <p class="card-text">${repo.description || 'Sem descrição disponível.'}</p>
-              <a href="repo.html?repo=${repo.name}" class="btn btn-primary" target="_blank">Ver repositório</a>
-            </div>
           </div>
-        </div>
       `).join('');
-  
+
       document.getElementById('projects-content').innerHTML = reposHTML;
-    } catch (error) {
+  } catch (error) {
       console.error('Erro ao obter dados dos repositórios:', error);
-    }
   }
-  
-  // Função para obter dados dos repositórios do usuário
-  async function getRepoData() {
+}
+
+  // Função para obter dados dos membros da equipe
+  async function getTeamData() {
     try {
-      const response = await fetch(userReposURL);
-      const reposData = await response.json();
-  
-      const reposHTML = reposData.map(repo => `
-        <div class="col-md-4 col-sm-6">
-          <div class="card">
-            <div class="card-body">
-              <h5 class="card-title">${repo.name}</h5>
-              <p class="card-text">${repo.description || 'Sem descrição disponível.'}</p>
-              <a href="repo.html?repo=${repo.name}" class="btn btn-primary" target="_blank">Ver repositório</a>
-            </div>
-          </div>
-        </div>
-      `).join('');
-  
-      document.getElementById('projects-content').innerHTML = reposHTML;
-    } catch (error) {
-      console.error('Erro ao obter dados dos repositórios:', error);
-    }
-  }
-  
-  
-    // Função para obter dados dos membros da equipe
-    async function getTeamData() {
-      try {
-        const teamData = await Promise.all(teamMemberURLs.map(async (url) => {
-          const response = await fetch(url);
-          return await response.json();
-        }));
-  
-        const teamHTML = teamData.map(member => `
+      const teamData = await Promise.all(teamMemberURLs.map(async (url) => {
+        const response = await fetch(url);
+        return await response.json();
+      }));
+
+      const teamHTML = teamData.map(member => `
         <div class="col-md-4">
           <div class="card">
             <img src="${member.avatar_url}" class="card-img-top" alt="Foto do membro da equipe">
@@ -91,46 +84,32 @@ document.addEventListener('DOMContentLoaded', () => {
           </div>
         </div>
       `).join('');
-  
-        document.getElementById('team-content').innerHTML = teamHTML;
-      } catch (error) {
-        console.error('Erro ao obter dados da equipe:', error);
-      }
+
+      document.getElementById('team-content').innerHTML = teamHTML;
+    } catch (error) {
+      console.error('Erro ao obter dados da equipe:', error);
     }
-  
-  
-    // Função para carregar imagens no carousel
-  // Exemplo simplificado para carregar dinamicamente o carrossel
-  document.addEventListener('DOMContentLoaded', () => {
-    const carouselImages = [
-      { src: '/public/assets/img/imagem-chatbot.png', alt: 'tecnologia', title: 'Chatbot', description: 'Descrição do chatbot.', link: 'https://example.com/chatbot' },
-      { src: '/public/assets/img/imagem-iot.png', alt: 'inovação', title: 'IoT', description: 'Descrição de IoT.', link: 'https://example.com/iot' },
-      // Adicione mais objetos conforme necessário
-    ];
-  
-    function loadCarouselContent() {
-      const carouselInner = document.querySelector('.carousel-inner');
-  
-      const carouselHTML = carouselImages.map((image, index) => `
-        <div class="carousel-item ${index === 0 ? 'active' : ''}">
-          <img src="${image.src}" class="d-block w-100" alt="${image.alt}">
-          <div class="carousel-caption d-none d-md-block">
-            <h5>${image.title}</h5>
-            <p>${image.description}</p>
-            <a href="${image.link}" class="btn btn-primary" target="_blank">Ver mais</a>
-          </div>
+  }
+
+  // Função para carregar imagens no carousel
+  function loadCarouselContent() {
+    const carouselInner = document.querySelector('.carousel-inner');
+
+    const carouselHTML = carouselImages.map((image, index) => `
+      <div class="carousel-item ${index === 0 ? 'active' : ''}">
+        <img src="${image.src}" class="d-block w-100" alt="${image.alt}">
+        <div class="carousel-caption d-none d-md-block">
+          <h5>Slide ${index + 1}</h5>
         </div>
-      `).join('');
-  
-      carouselInner.innerHTML = carouselHTML;
-    }
-   
-  });
-  
-  
-    // Carregar dados da página
-    getProfileData(userProfileURL, 'about-content');
-    getRepoData();
-    getTeamData();
-    loadCarouselContent();
-  });
+      </div>
+    `).join('');
+
+    carouselInner.innerHTML = carouselHTML;
+  }
+
+  // Inicialização: carregar dados do perfil, repositórios e equipe
+  getProfileData(userProfileURL, 'about-content');
+  getRepoData();
+  getTeamData();
+  loadCarouselContent();
+});
